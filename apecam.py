93833dc732c4e960da284ec62e8fa104d52f4526
Aran Wittert <aran@jobadder.com>
2021-04-28 23:46:42 +1000

    using httpx instead of requests

--- apecam.py
+++ apecam.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from browsermobproxy import Server
 import datetime
-import requests
+import httpx
 from moviepy.editor import VideoFileClip
 
 URL = 'https://zoo.sandiegozoo.org/cams/ape-cam'
@@ -26,10 +26,6 @@ proxy.new_har("ape-cam",options={'captureHeaders': True,'captureContent':True,'c
 driver = webdriver.Chrome('./chromedriver.exe', options=options, desired_capabilities=caps)
 driver.get(URL)
 
-#Time to wait before capture
-# print(f"\nWaiting to capture ~{sys.argv[1]} seconds of video\n")
-# proxy.wait_for_traffic_to_stop(10000, int(sys.argv[1])*1000)
-
 filename = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")+'.mpeg'
 
 fetched = []
@@ -48,21 +44,20 @@ try:
                 if 'text' not in _response['content'].keys() or not _response['content']['text']:
                     continue
                     
-                
-                r1 = requests.get(_url, stream=True)
-                if(r1.status_code == 200 or r1.status_code == 206):
-                    print(_url[45:]+'\n')
-        			
-                    #Re-open output file to append new video
-                    with open(f"./recordings/{filename}",'ab') as f:
-                        data = b''
-                        for chunk in r1.iter_content(chunk_size=1024):
-                            if(chunk):
-                                data += chunk
-                        f.write(data)
-                        fetched.append(_url)
-                else:
-                    print(f"Received unexpected status code {r1.status_code}")
+                with httpx.stream("GET", _url) as r1:
+                    if(r1.status_code == 200 or r1.status_code == 206):
+                        print(_url[45:]+'\n')
+            			
+                        #Re-open output file to append new video
+                        with open(f"./recordings/{filename}",'ab') as f:
+                            data = b''
+                            for chunk in r1.iter_bytes():
+                                if(chunk):
+                                    data += chunk
+                            f.write(data)
+                            fetched.append(_url)
+                    else:
+                        print(f"Received unexpected status code {r1.status_code}")
 except KeyboardInterrupt:
     pass
 
